b17f7c33f38072dd7c796559c7faff3f2cc6da47
sjkmost <sjkmoster@outlook.com>
2023-05-20 20:38:31 +0800

    initial project

---
+++ proj.srcs/sources_1/new/pipeliendcpu.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/05/20 20:34:52
+// Design Name: 
+// Module Name: pipeliendcpu
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pipeliendcpu(
+
+    );
+endmodule
